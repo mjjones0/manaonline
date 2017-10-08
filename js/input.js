2c4a4056f5b4eb473645e770ca333349554846e9
Solomon <root@solomon.(none)>
2017-10-08 02:00:22 -0400

    remove unneeded listener for gamepad

--- js/input.js
+++ js/input.js
@@ -31,17 +31,6 @@ GAME.Input = function()
 	} else {
 		setup_keyboard_buttons();
 	}
-
-	if(!!navigator.getGamepads){
-	    // Browser supports the Gamepad API
-	    	window.addEventListener("gamepadconnected", function(e) {
-	  var gp = navigator.getGamepads()[e.gamepad.index];
-	  console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-	    gp.index, gp.id,
-	    gp.buttons.length, gp.axes.length);
-	});
-	}
-
 }
 
 GAME.Input.constructor = GAME.Input;
